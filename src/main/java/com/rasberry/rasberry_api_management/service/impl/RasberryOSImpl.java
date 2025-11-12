90d1ffc7b60678eef925da29dc5fa2ad7a88c906
Roman Sergeev <roman.sergeev1993@gmail.com>
2025-11-13 01:09:41 +0300

    наработки + тестирование 19

--- src/main/java/com/rasberry/rasberry_api_management/service/impl/RasberryOSImpl.java
+++ src/main/java/com/rasberry/rasberry_api_management/service/impl/RasberryOSImpl.java
@@ -14,6 +14,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -45,6 +46,16 @@ public class RasberryOSImpl implements RcloneOSAction {
             Process process = null;
             log.info("cmd commands: {}", String.join(" ", processBuilder.command()));
             try {
+                processBuilder.environment().put("RCLONE_CONFIG", "/usr/bin/rclone/rclone.conf");
+
+                // Добавим -vv для диагностики на время
+                List<String> cmd = new ArrayList<>(processBuilder.command());
+                cmd.add("-vv");
+                processBuilder.command(cmd);
+
+                log.info("cmd: {}", String.join(" ", processBuilder.command()));
+
+
                 process = processBuilder.start();
 
 
